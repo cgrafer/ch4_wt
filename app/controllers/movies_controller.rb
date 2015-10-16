67185f027a33a1f115edc3a2a736448b0a3b9ef2
cgrafer1 <cgrafer1@uncc.edu>
2015-10-16 18:05:20 +0000

    finished update in section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,6 +16,20 @@ class MoviesController < ApplicationController
     # default: render 'new' template
   end
 
+# in movies_controller.rb
+ 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+   
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes(params.require(:movie).permit(:title, :description, :rating, :release_date))
+#    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
   # in movies_controller.rb
   def create
     @movie = Movie.create! movie_params
